159d45e140a1007e9087303bcc9db4dbbf69c91b
J <janbielowka@gmail.com>
2021-02-08 16:29:05 +0100

    actually WHO cares

--- Data/SpotifyAuth.cs
+++ Data/SpotifyAuth.cs
@@ -4,6 +4,6 @@ namespace SpotifyR
     {
         public string clientID = "826fdbe3b212461793a5024c3d89f96b";
         public string clientSecret = "b487b822b2704b4f81a4dcf2d4882412";
-        public string redirectURL = "https://localhost:5001/callback";
+        public string redirectURL = "https://spotify-recommendations.azurewebsites.net/callback";
     }
 }
